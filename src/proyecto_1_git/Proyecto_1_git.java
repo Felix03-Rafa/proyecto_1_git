834ebe3e47480a598a085ef916074f04c86d49a2
FELIX LARRAGA <FELIX LARRAGA@192.168.1.70>
2021-02-20 22:42:33 -0600

    este mi primer repositorio.

---
+++ src/proyecto_1_git/Proyecto_1_git.java
@@ -0,0 +1,16 @@
+
+package proyecto_1_git;
+
+
+public class Proyecto_1_git {
+
+    
+    public static void main(String[] args) {
+        int nunero;
+        
+        
+        
+        System.out.println("hola amigo");
+    }
+    
+}
